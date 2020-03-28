0744d7f106eb650af1cc28817f74a43cdd5c6feb
moerwald <andreas.mewald@gmail.com>
2020-03-28 12:17:33 +0100

    - Added SellIn test case

--- csharp.test/GildedRose/SellIn/Decrement/SellInDecrement.cs
+++ csharp.test/GildedRose/SellIn/Decrement/SellInDecrement.cs
@@ -1,14 +1,24 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace csharp.test.GildedRose.SellIn.Decrement
 {
     [TestFixture]
     public class SellInDecrement
     {
+        [Test]
+        public void SellIn_Decreases_Over_Time()
+        {
+            var sellIn = 3;
+            IList<Item> items = new List<Item>
+                {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = 50}};
+            var app = new csharp.GildedRose(items);
+            Enumerable.Range(1, sellIn).ToList().ForEach(i =>
+            {
+                app.UpdateQuality();
+                Assert.AreEqual(--sellIn, items[0].SellIn);
+            });
+        }
     }
 }
